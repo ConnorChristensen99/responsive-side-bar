d3d239cf6e91355d24680989f975881c4a95a741
Connor Christensen <connor.christensen@uvu.edu>
2023-08-18 09:40:48 -0600

    Done I guess

--- src/App.js
+++ src/App.js
@@ -12,40 +12,34 @@ import {
 	FaHeart,
 	FaCog,
 	FaSignInAlt,
-  FaBars
+	FaBars,
 } from "react-icons/fa";
-
 import "./App.css";
 
+
 function App() {
-  window.addEventListener("load", (event) => {
-    let close = document.getElementById('closeSide')
-    let open = document.getElementById('closeSideSmall')
-    close.addEventListener('click', () => {
-    
-      let bigNav = document.getElementById('sideBarContainer')
-      let smallNav = document.getElementById('sideBarContainerContainer')
+	window.addEventListener("load", (event) => {
+		let close = document.getElementById("closeSide");
+		let open = document.getElementById("closeSideSmall");
+		close.addEventListener("click", () => {
+			let bigNav = document.getElementById("sideBarContainer");
+			let smallNav = document.getElementById("sideBarContainerContainer");
 
-      if (smallNav.classList.contains('invisible')) {
-        bigNav.classList.add('invisible')
-        smallNav.classList.remove('invisible')
-      }  
-  })
-  open.addEventListener('click', () => {
-    let bigNav = document.getElementById('sideBarContainer')
-      let smallNav = document.getElementById('sideBarContainerContainer')
+			if (smallNav.classList.contains("invisible")) {
+				bigNav.classList.add("invisible");
+				smallNav.classList.remove("invisible");
+			}
+		});
+		open.addEventListener("click", () => {
+			let bigNav = document.getElementById("sideBarContainer");
+			let smallNav = document.getElementById("sideBarContainerContainer");
 
-    if (bigNav.classList.contains('invisible')) {
-      bigNav.classList.remove('invisible')
-      smallNav.classList.add('invisible')
-    }  
-})
-
-  });
-  
-
-
-
+			if (bigNav.classList.contains("invisible")) {
+				bigNav.classList.remove("invisible");
+				smallNav.classList.add("invisible");
+			}
+		});
+	});
 
 	return (
 		<div className="App">
@@ -53,14 +47,14 @@ function App() {
 				<h2>My Cool Responsive Side Bar</h2>
 				<img src={logo} className="App-logo" alt="logo" />
 			</header>
-      {/* BIG NAV */}
-			<div className="sideBarContainer" id='sideBarContainer'>
+			{/* BIG NAV */}
+			<div className="sideBarContainer" id="sideBarContainer">
 				<div className="sideBarHeader">
 					<span>
 						<FaAsymmetrik className="headerIcon" />
 						CodingLab
 					</span>
-					<FaTimes className="closeSideBar" id='closeSide'/>
+					<FaTimes className="closeSideBar" id="closeSide" />
 				</div>
 				<div className="sideBarSearch">
 					<FaSearch className="sideBarSearchIcon" />
@@ -68,7 +62,7 @@ function App() {
 				</div>
 				<nav classnmae="sideBar">
 					<ul className="navList">
-						<li>
+						<li className="active">
 							<span>
 								<FaBorderAll />
 							</span>
@@ -128,64 +122,68 @@ function App() {
 				</div>
 			</div>
 
-      {/* SMALL NAV */}
-      <div className='invisible' id='sideBarContainerContainer' title='Menu'>
-      <div className="sideBarContainer smallContainer" id='sideBarContainer'>
-				<div className="sideBarHeaderSmall">
-        <div className="searchBack"><FaBars className="closeSideBarSmall" id='closeSideSmall'/></div>
-				</div>
-				<div className="sideBarSearch" title='Search' >
-					<div className="searchBack"><FaSearch className="smallSearch" /></div>
-				</div>
-				<nav className="sideBarSmall">
-					<ul className="navListSmall">
-						<li>
-							<span title='dashboard'>
-								<FaBorderAll />
-							</span>
-						</li>
-						<li>
-							<span title='User'>
-								<FaUserAlt />
-							</span>
-						</li>
-						<li>
-							<span title='Messages'>
-								<FaRocketchat />
-							</span>
-						</li>
-						<li>
-							<span title='Analytics'>
-								<FaStackOverflow />
-							</span>
-						</li>
-						<li>
-							<span title='File Manager'>
-								<FaFolder />
-							</span>
-						</li>
-						<li>
-							<span title='Order'>
-								<FaShoppingCart />
-							</span>
-						</li>
-						<li>
-							<span title='Saved'>
-								<FaHeart />
-							</span>
-						</li>
-						<li>
-							<span title='Settings'>
-								<FaCog />
-							</span>
-						</li>
-					</ul>
-				</nav>
-				<div className="sideBarBottom">
-					<FaSignInAlt className="signOutSmall" />
+			{/* SMALL NAV */}
+			<div className="invisible" id="sideBarContainerContainer" title="Menu">
+				<div className="sideBarContainer smallContainer" id="sideBarContainer">
+					<div className="sideBarHeaderSmall">
+						<div className="searchBack">
+							<FaBars className="closeSideBarSmall" id="closeSideSmall" />
+						</div>
+					</div>
+					<div className="sideBarSearch" title="Search">
+						<div className="searchBack">
+							<FaSearch className="smallSearch" />
+						</div>
+					</div>
+					<nav className="sideBarSmall">
+						<ul className="navListSmall">
+							<li className="active">
+								<span title="dashboard">
+									<FaBorderAll />
+								</span>
+							</li>
+							<li>
+								<span title="User">
+									<FaUserAlt />
+								</span>
+							</li>
+							<li>
+								<span title="Messages">
+									<FaRocketchat />
+								</span>
+							</li>
+							<li>
+								<span title="Analytics">
+									<FaStackOverflow />
+								</span>
+							</li>
+							<li>
+								<span title="File Manager">
+									<FaFolder />
+								</span>
+							</li>
+							<li>
+								<span title="Order">
+									<FaShoppingCart />
+								</span>
+							</li>
+							<li>
+								<span title="Saved">
+									<FaHeart />
+								</span>
+							</li>
+							<li>
+								<span title="Settings">
+									<FaCog />
+								</span>
+							</li>
+						</ul>
+					</nav>
+					<div className="sideBarBottom">
+						<FaSignInAlt className="signOutSmall" />
+					</div>
 				</div>
 			</div>
-      </div>
 		</div>
 	);
 }
